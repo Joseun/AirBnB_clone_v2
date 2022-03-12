c7df37e5d1ffb87a52b0c59ab4627b3c9a8f64ea
Joseun <ologanj@gmail.com>
2022-03-12 23:50:38 +0000

    corrected setup files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Creates the database hbnb_test_db and the user hbnb_test
 -- The hbnb_test has all privileges on hbnb_test_db with password hbnb_test_pwd
---- The hbnb_test has SELECT privilege on performance_schema with password hbnb_test_pwd
+-- The hbnb_test has SELECT privilege on performance_schema with password hbnb_test_pwd
 CREATE DATABASE
     IF NOT EXISTS `hbnb_test_db`;
 CREATE USER
